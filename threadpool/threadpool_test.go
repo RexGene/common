8a2f8efbf16e2031995135ccb95e1b57e343ee10
RexGene <rexgene@hotmail.com>
2016-05-13 18:32:27 +0800

    rename

---
+++ threadpool/threadpool_test.go
@@ -0,0 +1,22 @@
+package threadpool
+
+import (
+	"testing"
+)
+
+type TestTask struct {
+}
+
+func (self TestTask) onExecute() {
+	print("test")
+}
+
+func TestStart(t *testing.T) {
+	pool := New()
+	task := new(TestTask)
+	for i := 0; i < 1000; i++ {
+		pool.Start(task)
+	}
+
+	pool.WaitAllFinish()
+}
